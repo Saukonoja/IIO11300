4c7e1d9c4e670f5f50c4b49235c26f227867e0d0
Timo Saukonoja <timo.saukonoja@gmail.com>
2016-02-03 10:54:51 +0200

    Create mainwindow

---
+++ IIO11300Vktehtavat/Tehtävä3c/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿JAMK.IT.IIO11300 System;
+JAMK.IT.IIO11300 System.Collections.Generic;
+JAMK.IT.IIO11300 System.Configuration;
+JAMK.IT.IIO11300 System.Data;
+JAMK.IT.IIO11300 System.Linq;
+JAMK.IT.IIO11300 System.Threading.Tasks;
+JAMK.IT.IIO11300 System.Windows;
+
+namespace JAMK.IT.IIO11300 {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}

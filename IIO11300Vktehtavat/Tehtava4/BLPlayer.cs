e6e08031d803cc9bb3e3910fd5f04d953d739908
Timo Saukonoja <timo.saukonoja@gmail.com>
2016-02-10 10:39:36 +0200

    Create mainwindow

---
+++ IIO11300Vktehtavat/Tehtava4/BLPlayer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace JAMK.IT.IIO11300 {
+    class BLPlayer {
+
+        #region Variables
+        private string fname;
+        private string lname;
+        private int price;
+        private string team;
+        #endregion
+
+        #region Properties
+        public string Fname {
+            get { return fname;}
+            set { fname = value;}
+        }
+        public string Lname {
+            get { return lname; }
+            set { lname = value; }
+        }
+        public int Price {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public string Team {
+            get { return team; }
+            set { team = value; }
+        }
+        #endregion
+
+        #region Constructors
+        public BLPlayer() { }
+        public BLPlayer(string fname, string lname, int price, string team) {
+            this.fname = fname;
+            this.lname = lname;
+            this.price = price;
+            this.team = team;
+        }
+        #endregion
+
+        #region Methods
+        public static void SaveToFile(List<BLPlayer> players) {
+            try {
+                StreamWriter sw;
+                //tarkastetaan onko tiedosto jo olemassa
+                if (File.Exists(filename)) {
+                    //liitetään olemassa olevaan tiedostoon
+                    sw = File.AppendText(filename);
+                } else {
+                    //luodaan uusi
+                    sw = File.CreateText(filename);
+                }
+                //kirjoitus
+                foreach (var item in players) {
+                    sw.WriteLine(item);
+                }
+                sw.Close();
+            } catch (Exception ex) {
+
+                throw ex;
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ IIO11300Vktehtavat/Tehtava4/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JAMK.IT.IIO11300 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+           
+            List<BLPlayer> players = new List<BLPlayer>();
+            public MainWindow() {
+            InitializeComponent();
+           string []teams = new string[] { "Blues", "HIFK", "HPK", "Ilves", "JYP", "KalPa", "KooKoo",
+            "Kärpät", "Lukko", "Pelicans", "SaiPa", "Sport", "Tappara", "TPS", "Ässät" };
+            cbTeam.ItemsSource = teams;
+
+        }
+
+        private void btnCreatePlayer_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void btnSavePlayer_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void btnDeletePlayer_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void btnWritePlayers_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void btnQuit_Click(object sender, RoutedEventArgs e) {
+            Application.Current.Shutdown();
+        }
+
+        private void cbTeam_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+        }
+    }
+}

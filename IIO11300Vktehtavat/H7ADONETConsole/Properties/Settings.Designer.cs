3913cb8eedf6adb4dc009333a93b2587473e305a
Timo Saukonoja <timo.saukonoja@gmail.com>
2016-03-10 10:53:24 +0200

    Add H7ADONETConsole

--- IIO11300Vktehtavat/H7ADONETConsole/Properties/Settings.Designer.cs
+++ IIO11300Vktehtavat/H7ADONETConsole/Properties/Settings.Designer.cs
@@ -8,18 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Tehtava7.Properties {
-
-
+namespace H7ADONETConsole.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
+        
         public static Settings Default {
             get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data source=eight.labranet.jamk.fi; initial catalog=course; user=koodari; passwor" +
+            "d=koodari13")]
+        public string Tietokanta {
+            get {
+                return ((string)(this["Tietokanta"]));
             }
         }
     }

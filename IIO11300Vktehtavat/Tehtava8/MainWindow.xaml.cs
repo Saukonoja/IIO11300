7092b16da03099e3c7f3d19b471c9aeeba36f13b
Timo Saukonoja <h3694@student.jamk.fi>
2016-03-16 17:24:40 +0200

    Finish tehtava8

--- IIO11300Vktehtavat/Tehtava8/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/Tehtava8/MainWindow.xaml.cs
@@ -12,18 +12,53 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Tehtava8 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+   
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
         }
 
         private void btnGetCustomers_Click(object sender, RoutedEventArgs e) {
+            try {
+                //yhteys
+                using (SqlConnection conn = new SqlConnection(Tehtava8.Properties.Settings.Default.Tietokanta)) {
+                    string sql = "SELECT firstname, lastname, address, city FROM vCustomers GROUP BY lastname, firstname, address, city";
+                    //dataadapter
+                    SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+                    DataTable dt = new DataTable("winecustomers");
+                    da.Fill(dt);
+                    //sidotaan datatable listboxiin
+                    lbCustomers.DataContext = dt;
+                    lbCustomers.DisplayMemberPath = "lastname";
+                    conn.Close();
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void lbCustomers_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            DataRowView drv = lbCustomers.SelectedItem as DataRowView;
+            string firstname = "";
+            string lastname = "";
+            string address = "";
+            string city = "";
 
+            if (drv != null) {
+                firstname = drv.Row[lbCustomers.DisplayMemberPath = "firstname"] as string;
+                lastname = drv.Row[lbCustomers.DisplayMemberPath = "lastname"] as string;
+                address = drv.Row[lbCustomers.DisplayMemberPath = "address"] as string;
+                city = drv.Row[lbCustomers.DisplayMemberPath = "city"] as string;
+            }
+            lbCustomers.DisplayMemberPath = "lastname";
+            txtFname.Text = firstname;
+            txtLname.Text = lastname;
+            txtAddress.Text = address;
+            txtCity.Text = city;
         }
     }
 }
